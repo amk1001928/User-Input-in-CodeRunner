5385788c6ab13322b5cbe84f4ad9b6f62ec7e199
Your Name <your.email@example.com>
2024-09-09 10:18:55 +0300

    removed the justin

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("What is your name?");
+        System.out.println("What is your name");
 
-        String name = "Justin";
+        String name = "Peter";
         System.out.println("Your name is "+name);
     
     }
