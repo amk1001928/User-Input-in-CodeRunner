4898a049da38cfe9f90f639dafda2d53d56c00a1
Your Name <your.email@example.com>
2024-09-09 10:04:12 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("What is your name?");
+
+        String name = "Justin";
+        System.out.println("Your name is "+name);
+    
+    }
+}
